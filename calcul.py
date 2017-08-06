b6b8a3054492bac71d20329b7d80e809de15db62
ehbc221 <ehbc221@gmail.com>
2017-08-06 21:04:23 +0000

    changed theme and added envoi_sms

--- calcul.py
+++ calcul.py
@@ -1,137 +1,139 @@
 from math import *
-def CreeMatCar(n):
+import requests
+import json
+
+
+def creer_matrice_carree(n):
     return [[0.0 for j in range(n)] for i in range(n)]
 
-def CreerListe(n):
-	return [0.0 for i in range(n)]
+
+def creer_liste(n):
+    return [0.0 for i in range(n)]
 
 
-def recherchepivot(k,A, n):
+def rechercher_pivot_gauss(k, A, n):
     i = k
     while i != n and A[i][k] == 0:
-        i+=1
+        i += 1
     if i != n:
         return i
     else:
         return -1
 
-def permutation(i0,i,A,b):
-    A[i],A[i0] = A[i0],A[i]
-    b[i],b[i0] = b[i0],b[i]
-    return A,b
 
-def elimination(k,A,b, n):
-    i=k+1
+def permutation_gauss(i0, i, A, b):
+    A[i], A[i0] = A[i0], A[i]
+    b[i], b[i0] = b[i0], b[i]
+    return A, b
+
+
+def elimination_gauss(k, A, b, n):
+    i = k+1
     while i < n:
         r = A[i][k]/A[k][k]        
         b[i] -= r*b[k]
-        j=k
+        j = k
         while j < n:
             A[i][j] -= r*A[k][j]
-            j+=1
-        i+=1    
-    return A,b
+            j += 1
+        i += 1
+    return A, b
 
-def remonte(A,b, n):
+
+def remontee_gauss(A, b, n):
     x = []
     i = 0
-    while i<n:
+    while i < n:
         x.append(0)
-        i+=1        
+        i += 1
     x[n-1] = b[n-1]/A[n-1][n-1]
     i = n-2
-    while i>=0:    
+    while i >= 0:
         x[i] = b[i]            
         j = i+1
-        while j<n:
+        while j < n:
             x[i] -= A[i][j]*x[j]
-            j+=1            
-        x[i]/=A[i][i]
-        i-=1
+            j += 1
+        x[i] /= A[i][i]
+        i -= 1
     return x
 
-def Gauss(A, b, n):
-    
+
+def resolution_gauss(A, b, n):
     k = 0
     arret = 0
-    while k!=n and arret!=1:
-        i0 = recherchepivot(k,A,n)
+    while k != n and arret != 1:
+        i0 = rechercher_pivot_gauss(k, A, n)
         if i0 == -1:
             arret = 1
         else:
             if i0 == k:
-                A,b = elimination(k,A,b,n)
-                k+=1
+                A, b = elimination_gauss(k, A, b, n)
+                k += 1
             else: 
-                A,b = permutation(i0,k,A,b)
-                A,b = elimination(k,A,b,n)
-                k+=1
-    if k==n and A[n-1][n-1]!=0:
-        # equation solutions
-        x = remonte(A,b,n)
+                A, b = permutation_gauss(i0, k, A, b)
+                A, b = elimination_gauss(k, A, b, n)
+                k += 1
+    if k == n and A[n-1][n-1] != 0:
+        # solutions de l'équation
+        x = remontee_gauss(A, b, n)
         for i in range(n):
-            x[i]=round(x[i],4)
+            x[i] = round(x[i], 4)
         result = ""
         for i in range(n):
             if (i != n) & (i != 0):
-                result +="\n"
+                result += "\n"
             result += "x{} = {}".format(i+1, x[i])          
     else:
-        # no unique solution
+        # pas de solution unique
         result = "0"
     return result
 
-def factCholesky(A):
-	n=len(A)
-	B=CreeMatCar(n)
 
-	for j in range(n):
-		B[j][j]=A[j][j]
-		for k in range(j):
-			B[j][j] -=B[j][k]*B[j][k]
-		B[j][j]= round( sqrt(abs(B[j][j])), 2)
-		for i in range(j+1,n):
-			B[i][j]=A[i][j]
-			for k in range(j):
-				B[i][j]-=B[i][k]*B[j][k]
-			B[i][j]= round(B[i][j]/B[j][j],2)
-
-	return B
-
+def methode_factorisation_cholesky(A):
+    n = len(A)
+    B = creer_matrice_carree(n)
+    for j in range(n):
+        B[j][j]=A[j][j]
+        for k in range(j):
+            B[j][j] -= B[j][k]*B[j][k]
+        B[j][j] = round(sqrt(abs(B[j][j])), 2)
+        for i in range(j+1, n):
+            B[i][j] = A[i][j]
+            for k in range(j):
+                B[i][j] -= B[i][k]*B[j][k]
+            B[i][j] = round(B[i][j]/B[j][j], 2)
+    return B
 
 
-
-def facto_LU(A,L,U):
-    ok=0
+"""
+def factorisation_lu(A,L,U):
+    ok = 0
     for i in range(n):
         U[0][i] = A[0][i]
         for j in range(n):
-            if i<=j:
-                L[i][j]=0
+            if i <= j:
+                L[i][j] = 0
                 s = float(0)
                 for k in range(i):
                     s += L[i][k]*U[k][j]
-                U[i][j]= A[i][j] - s
+                U[i][j] = A[i][j] - s
             else:
-                U[i][j]=0
+                U[i][j] = 0
                 t = float(0)
                 for k in range(j):
                     t += L[i][k]*U[k][j]
-                if U[j][j] !=0:
+                if U[j][j] != 0:
                     L[i][j] = (A[i][j] - t)/U[j][j]
                 else:
-                    ok=1
+                    ok = 1
     for i in range(n):
         L[i][i] = 1
-    return L,U,ok
+    return L, U, ok
+"""
 
 
-
-def CreeMatCar(n):
-    return [[0.0 for j in range(n)] for i in range(n)]
-
-
-def eliminationLU(k, A):
+def elimination_lu(k, A):
     i = k + 1
     while i < n:
         r = A[i][k] / A[k][k]
@@ -143,29 +145,31 @@ def eliminationLU(k, A):
         i += 1
     return A
 
-def factLU(A):
+
+def methode_factorisation_lu(A):
     global n
     n = len(A)
     k = 0
     arret = 0
-    L = CreeMatCar(n)
+    L = creer_matrice_carree(n)
     while k != n and arret != 1:
         L[k][k] = 1
         if A[k][k] != 0:
             for i in range(k + 1, n):
                 L[i][k] = round(A[i][k] / A[k][k], 2)
-            A = eliminationLU(k, A)
+            A = elimination_lu(k, A)
             k += 1
         else:
             arret = 1
-    if arret == 0 and A[n - 1][n - 1] != 0:
+    if arret == 0 and A[n-1][n-1] != 0:
         return L
     else:
         print("Les conditions ne sont pas reunies")
         return 0
 
-def descente(L, b):
-    y = CreerListe(n)
+
+def descente_lu(L, b):
+    y = creer_liste(n)
     for i in range(n):
         y[i] = b[i]
         for j in range(i):
@@ -173,9 +177,8 @@ def descente(L, b):
     return y
 
 
-
-def remonte(A, b):
-    x = CreerListe(n)
+def remontee_lu(A, b):
+    x = creer_liste(n)
     x[n - 1] = round(b[n - 1] / A[n - 1][n - 1], 3)
     for i in range(n - 2, -1, -1):
         x[i] = b[i]
@@ -186,13 +189,27 @@ def remonte(A, b):
     return x
 
 
-def solveSystLU(A, b):
-    L = factLU(A)
+def methode_resolution_lu(A, b):
+    L = methode_factorisation_lu(A)
     if L != 0:
-        y = descente(L, b)
-        x = remonte(A, y)
+        y = descente_lu(L, b)
+        x = remontee_lu(A, y)
         # print("La solution de votre systeme est \n",x)
         return x
     else:
         # print("Ce systeme ne peut pas etre resolu par la methode LU")
         return 0
+
+
+def envoi_sms(number,message):
+    url="http://smsgateway.me/api/v3/messages/send"
+    payload = {
+        'email': 'ehbc221@gmail.com',
+        'password': 'pengriffey',
+        'device': 55955,
+        'number': '+221778659165',
+        'message': 'TEST 123'
+    }
+    result = requests.post(url, payload)
+    result = result.json()
+    return result['success'] and len(result['result']['fails']) == 0
--- main.py
+++ main.py
@@ -1,78 +1,89 @@
 from flask import Flask, render_template, url_for, request, redirect
 from calcul import *
+
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/accueil')
-@app.route('/home')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/resolution_lu')
+def resolution_lu():
+    if request.args.get('taille') is None:
+        return render_template('resolution_lu.html')
+    t = int(request.args.get('taille'))
+    return render_template('resolution_lu.html', taille=t)
+
+
+@app.route('/resolution_lu/resultats', methods=['GET', 'POST'])
+def resolution_lu_results():
+    if request.method != 'POST':
+        return redirect(url_for('resolution_lu'))
+    taille = int(request.form['taille'])
+    A = creer_matrice_carree(taille)
+    b = creer_liste(taille)
+    for i in range(0, taille):
+        for j in range(0, taille):
+            A[i][j] = float(request.form['A'+str(i)+'_'+str(j)])
+    for k in range(0, taille):
+        b[k] = float(request.form['b'+str(k)])
+    x = creer_liste(taille)
+    x = methode_resolution_lu(A, b)
+    envoi_sms('778659165', 'hello world')
+    return render_template('resolution_lu_resultat.html', taille=taille, x=x)
+
+
+@app.route('/factorisation_lu')
+def factorisation_lu():
+    if request.args.get('taille') is None:
+        return render_template('factorisation_lu.html')
+    t = int(request.args.get('taille'))
+    return render_template('factorisation_lu.html', taille=t)
+
+
+@app.route('/factorisation_lu/resultats', methods=['GET', 'POST'])
+def factorisation_lu_results():
+    if request.method != 'POST':
+        return redirect(url_for('factorisation_lu'))
+    taille = int(request.form['taille'])
+    A = creer_matrice_carree(taille)
+    for i in range(0, taille):
+        for j in range(0, taille):
+            A[i][j] = float(request.form['A'+str(i)+'_'+str(j)])
+    L = creer_matrice_carree(taille)
+    L = methode_factorisation_lu(A)
+    return render_template('factorisation_lu_results.html', L=L, taille=taille)
+
+
+@app.route('/factorisation_cholesky')
+def factorisation_cholesky():
+    if request.args.get('taille') is None:
+        return render_template('factorisation_cholesky.html')
+    t = int(request.args.get('taille'))
+    return render_template('factorisation_cholesky.html', taille=t)
+
+
+@app.route('/factorisation_cholesky/resultats', methods=['GET', 'POST'])
+def factorisation_cholesky_results():
+    if request.method != 'POST':
+        return redirect(url_for('factorisation_cholesky'))
+    taille = int(request.form['taille'])
+    A = creer_matrice_carree(taille)
+    for i in range(0, taille):
+        for j in range(0, taille):
+            A[i][j] = float(request.form['A'+str(i)+'_'+str(j)])
+    B = creer_matrice_carree(taille)
+    B = methode_factorisation_cholesky(A)
+    return render_template('factorisation_cholesky_results.html', B=B, taille=taille)
+
 
 @app.route('/aide')
 def aide():
-	return render_template('aide.html')
-@app.route('/resol_lu')
-def resol_lu():
-	if request.args.get('taille') is None:
-		return render_template('resol_lu.html')
-	t = int(request.args.get('taille'))
-	return render_template('resol_lu.html', taille=t)
+    return render_template('aide.html')
 
-@app.route('/resol_lu/results', methods=['GET', 'POST'])
-def resol_lu_results():
-	if request.method != 'POST':
-		return redirect(url_for('resol_lu'))
-	taille = int(request.form['taille'])
-	A = CreeMatCar(taille)
-	b = CreerListe(taille)
-	for i in range(0,taille):
-		for j in range(0,taille):
-			A[i][j] = float(request.form['A'+str(i)+'_'+str(j)])
-	for k in range(0,taille):
-		b[k] = float(request.form['b'+str(k)])
-	x = CreerListe(taille)
-	x = solveSystLU(A, b)
-	return render_template('resol_lu_results.html', taille=taille, x=x)
-
-@app.route('/fact_lu')
-def fact_lu():
-	if request.args.get('taille') is None:
-		return render_template('fact_lu.html')
-	t = int(request.args.get('taille'))
-	return render_template('fact_lu.html', taille=t)
-
-@app.route('/fact_lu/results', methods=['GET', 'POST'])
-def fact_lu_results():
-	if request.method != 'POST':
-		return redirect(url_for('fact_lu'))
-	taille = int(request.form['taille'])
-	A = CreeMatCar(taille)
-	for i in range(0,taille):
-		for j in range(0,taille):
-			A[i][j] = float(request.form['A'+str(i)+'_'+str(j)])
-	L = CreeMatCar(taille)
-	L = factLU(A)
-	return render_template('fact_lu_results.html', L=L, taille=taille)
-
-@app.route('/fact_chol')
-def fact_chol():
-	if request.args.get('taille') is None:
-		return render_template('fact_chol.html')
-	t = int(request.args.get('taille'))
-	return render_template('fact_chol.html', taille=t)
-
-@app.route('/fact_chol/results', methods=['GET', 'POST'])
-def fact_chol_results():
-	if request.method != 'POST':
-		return redirect(url_for('fact_chol'))
-	taille = int(request.form['taille'])
-	A = CreeMatCar(taille)
-	for i in range(0,taille):
-		for j in range(0,taille):
-			A[i][j] = float(request.form['A'+str(i)+'_'+str(j)])
-	B = CreeMatCar(taille)
-	B = factCholesky(A)
-	return render_template('fact_chol_results.html', B=B, taille=taille)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
